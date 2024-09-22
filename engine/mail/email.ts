64644798de1574cae3d1558f5279d0465705dbd0
kosmixyt <flo.cl25spt@gmail.com>
2024-09-22 12:50:25 +0200

    add email controller and account

--- engine/mail/email.ts
+++ engine/mail/email.ts
@@ -1,9 +1,16 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
-
-
+import { Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class EmailController{
-    @PrimaryGeneratedColumn('increment')
-    id! : number
+export class EmailController {
+  @PrimaryGeneratedColumn("increment")
+  id!: number;
+  @ManyToOne(() => EmailAccount, (acc) => acc.controller)
+  accounts!: EmailAccount[];
+}
+@Entity()
+export class EmailAccount {
+  @PrimaryGeneratedColumn("increment")
+  id!: number;
+  @OneToMany(() => EmailController, (controller) => controller.accounts)
+  controller!: EmailController;
 }
